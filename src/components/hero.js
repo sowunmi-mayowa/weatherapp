1b29e4b7a1e6ac24ee7064f293f6ae9646e0019a
sowunmi mayowa <smayowa176@gmail.com>
2023-03-24 09:04:11 +0100

    implemented local time

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,11 @@
 import {useEffect, useState} from 'react';
+import heroBg from '../img/pexels-sebastian-arie-voortman-165505.jpg';
+import Header from './header';
+import { useClock } from 'react-use-clock'
 
-const Hero = () => {
-    const [img, setImg] = useState(null)
+const Hero = (props) => {
+    console.log(props)
+    const [img, setImg] = useState(null);
     function getBg(){
         fetch('https://api.unsplash.com/photos/random/?client_id=GB-b7rfszFfYynFsFNIa7_vXYN35zaPIyeceyOF0OOY')
         .then(res => res.json())
@@ -9,16 +13,30 @@ const Hero = () => {
         .catch(err => console.log(err))
     }
 
-    useEffect(() => {
-        getBg()
-    })
+    // useEffect(() => {
+    //     getBg()
+    // })
+    function getDate(){
+        let date = new Date();
+
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+
+        return `${day}-${month}-${year}`
+    }
+     const clock = useClock()
     return (
-        <div>
-            {
-                img && (
-                    <img src={img.links.html} alt="img"/>
-                )
-            }
+        <div style={{background: `url(${heroBg})`, backgroundPosition: "center", backgroundSize: "cover", height: "80vh"}} className="flex justify-center items-center flex-col">
+            <Header />
+			<strong className="text-6xl text-white">
+				{clock.hours.toString().padStart(2, '0')}:
+				{clock.minutes.toString().padStart(2, '0')}:
+				{clock.seconds.toString().padStart(2, '0')}
+			</strong>
+            <div className="text-white">
+                <p className="text-center text-2xl font-thin"> { getDate()}</p>
+            </div>
         </div>
     )
 }
